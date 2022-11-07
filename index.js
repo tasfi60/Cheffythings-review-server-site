b1a4d10e78b0f75a15f4888f5814bc4af9fd26ed
tasfi60 <tasfiagirl@gmail.com>
2022-11-08 02:26:02 +0600

    fifth commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/',(req,res) => {
     res.send('Simple node Server is Running');
 });
 
+app.use(cors());
+
 app.listen(port, () =>{
     console.log(`Simple node server is running on ${port}`);
 })
