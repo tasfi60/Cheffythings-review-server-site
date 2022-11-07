09a8ccfeb892ce4cb57039cffdc0f0c4d9d35ba6
tasfi60 <tasfiagirl@gmail.com>
2022-11-08 02:19:07 +0600

    Third commit

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/',(req,res) => {
+    res.send('Simple node Server is Running');
+})
